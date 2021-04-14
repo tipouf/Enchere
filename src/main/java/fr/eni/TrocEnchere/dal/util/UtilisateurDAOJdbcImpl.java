dcdb32b2f465a1ef2a67d947c20e4d8f0c7a3c64
cindon <nicolascindon@gmail.com>
2021-04-14 09:08:54 +0200

    profil

--- src/main/java/fr/eni/TrocEnchere/dal/util/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/TrocEnchere/dal/util/UtilisateurDAOJdbcImpl.java
@@ -79,6 +79,29 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 	}
 
 	@Override
+	public void update(Utilisateur utilisateur)  throws BusinessException {
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+
+			PreparedStatement pStmt = cnx.prepareStatement(UPDATE);
+			pStmt.setString(1, utilisateur.getPseudo().toString());
+			pStmt.setString(2, utilisateur.getNom().toString());
+			pStmt.setString(3, utilisateur.getPrenom().toString());
+			pStmt.setString(4, utilisateur.getEmail().toString());
+			pStmt.setString(5, utilisateur.getTelephone().toString());
+			pStmt.setString(6, utilisateur.getRue().toString());
+			pStmt.setString(7, utilisateur.getCodePostal().toString());
+			pStmt.setString(8, utilisateur.getVille().toString());
+			pStmt.setInt(9, utilisateur.getNoUtilisateur());
+
+			pStmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}	
+
+
+	@Override
 	public ArrayList<Utilisateur> getAll() {
 
 		// Liste des utilisateurs
---
+++ src/main/java/fr/eni/TrocEnchere/servlets/ServletModifierProfil.java
@@ -0,0 +1,80 @@
+package fr.eni.TrocEnchere.servlets;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.TrocEnchere.bll.UtilisateurManager;
+import fr.eni.TrocEnchere.bo.Utilisateur;
+
+
+@WebServlet("/ServletModifierProfil")
+public class ServletModifierProfil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = null;
+		String error = null;
+
+		UtilisateurManager utilisateurManager = new UtilisateurManager();
+
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String codePostal = request.getParameter("codePostal");
+		String ville = request.getParameter("ville");
+
+		HttpSession session = request.getSession(true);    
+		int idSession = (int) session.getAttribute("user_id");
+		String pseudoSession = (String) session.getAttribute("pseudo");
+		String emailSession = (String) session.getAttribute("email");
+
+		
+		System.out.println("pseudo " + pseudo);
+		System.out.println("pseudoSession " + pseudoSession);
+		if (!pseudo.equalsIgnoreCase(pseudoSession) ) {
+			if (!utilisateurManager.isPseudoAvailable(pseudo)) {
+
+				error = "Le pseudo existe d�j�";
+			}
+
+		} else if (!email.equalsIgnoreCase(emailSession)) {
+			if(!utilisateurManager.isEmailAvailable(email)) {
+			}
+			error = "L'email existe d�j�";
+		}
+
+	Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, idSession);
+	System.out.println("utilisateur " + utilisateur);
+	// Redirige vers la page inscription avec un message d'erreur
+	if (error != null) {
+		request.setAttribute("error", error);
+		request.setAttribute("monProfil", utilisateur);
+		rd = request.getRequestDispatcher("/WEB-INF/UtilisateurModifierProfil.jsp");
+		rd.forward(request, response);
+	}
+
+
+	try {
+		utilisateurManager.modifier(utilisateur);
+	} catch (Exception e) {}
+
+	rd = request.getRequestDispatcher("./ServletProfil");
+	rd.forward(request, response);
+}
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	doGet(request, response);
+}
+}
